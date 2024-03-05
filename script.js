d23640763691443bf7b5a736d7be53d469091999
Ross Clay <clay.rossclay@gmail.com>
2024-03-05 12:58:20 -0500

    replaces 16x16 btn with custom

--- script.js
+++ script.js
@@ -1,29 +1,15 @@
 // Etch-A-Sketch!
 // Create a 16x16 grid using javascript
 // Each grid item will have hover effect causing divs to change color
+// Allows custom size based on user input (grid should always be perfect square, so no need to distinguish width and height)
+// BONUS 1: Adds button to change coloring to "rainbow mode" (aka, applies random RGB background-color on hover)
+// BONUS 2: Implements a progressive darkening effect, with each hover making the square 10% darker, with 10 hovers resulting in pitch black
 
 let squareContainer = document.querySelector('.square-container')
 
+// separately create squares and effects into multiple functions
 
-// Create the grid with a button
-let sixteenBySixteenBtn = document.querySelector('.sixteen-by-sixteen')
-
-// // create square ->(grid) and hover effect in one function?
-// sixteenBySixteenBtn.addEventListener('click', () => {
-//     let div = document.createElement('div')
-//     div.setAttribute('class', 'square')
-//     squareContainer.appendChild(div)
-//     squares.forEach((square) => square.addEventListener('mouseenter', () => {
-//         square.setAttribute('style', 'background-color: black');
-//     }))
-
-// }
-// )
-
-
-// separate create squares and effects into multiple functions
-sixteenBySixteenBtn.addEventListener('click', createSquares(16, 16))
-
+createSquares(16, 16)
 function createSquares(width, height) {
     for (i = 0; i < height; i++) {
         let squareRowDiv = document.createElement('div')
@@ -35,8 +21,6 @@ function createSquares(width, height) {
             squareRowDiv.appendChild(squareDiv)
         }
     }
-
-
 }
 
 // squares change color on hover 
@@ -45,10 +29,12 @@ squares.forEach((square) => square.addEventListener('mouseenter', () => {
     square.setAttribute('style', 'background-color: black');
 }))
 
-
-
 // clear button
 let clearBtn = document.querySelector('.clear')
 clearBtn.addEventListener('click', () =>
     squares.forEach((square) => square.setAttribute('style', 'background-color: #eee'))
 )
+
+// // custom grid size, up to 100x100 (UNDER CONSTRUCTION)
+// let customBtn = document.querySelector('.custom')
+// customBtn.addEventListener('click', createSquares(int(prompt('Please enter a value between 1-100:')))
